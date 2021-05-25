b3d3ddebd055d76ecc68da8dacb3e20849b08847
hhsieh14 <80777858+hhsieh14@users.noreply.github.com>
2021-05-25 16:24:09 +0800

    Create SearchInsertPosition.py
    1. look at first and last element.
    2. check the array

---
+++ SearchInsertPosition.py
@@ -0,0 +1,9 @@
+class Solution:
+    def searchInsert(self, nums: List[int], target: int) -> int:
+        if target <= nums[0]:
+            return 0
+        if target > nums[-1]:
+            return len(nums)
+        for i in range(len(nums)):
+            if nums[i] < target and target <=nums[i+1] :
+                return i+1
