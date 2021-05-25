0ca1d2dd4afeafccca9a6f3dd6c4097653af2425
hhsieh14 <80777858+hhsieh14@users.noreply.github.com>
2021-05-25 15:55:09 +0800

    check if subString is matched

---
+++ ImplementstrStr().py
@@ -0,0 +1,12 @@
+class Solution:
+    def strStr(self, haystack: str, needle: str) -> int:
+    
+        if needle == "" or haystack == needle:
+            return 0
+        
+        n = len(needle) - 1
+        index = 0
+        for i in range(len(haystack) - n):
+            if haystack[i:i+n+1] == needle:
+                return i
+        return -1
