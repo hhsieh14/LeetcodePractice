3e7c6f08ad228c4b7ba39c6e9aba7960606e2203
hhsieh14 <80777858+hhsieh14@users.noreply.github.com>
2021-09-04 20:31:58 -0700

    peak finding using binary search

---
+++ FindPeakElement.py
@@ -0,0 +1,20 @@
+class Solution(object):
+    def findPeakElement(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: int
+        """
+        left = 0
+        right = len(nums) - 1
+        while left < right:
+            mid = int((left + right)/2)
+            if nums[mid] < nums[mid + 1]:
+                left = mid + 1
+            else:
+                right = mid
+            
+            
+        return left
+      
+####O(log(n))####
+###peak finding using binary search ####
