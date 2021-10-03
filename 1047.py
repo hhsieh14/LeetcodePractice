d4a4c416c2d235b0b855b5d93e35adbb0bd61483
hhsieh14 <80777858+hhsieh14@users.noreply.github.com>
2021-10-02 23:06:18 -0700

    Create 1047.py

---
+++ 1047.py
@@ -0,0 +1,13 @@
+class Solution(object):
+    def removeDuplicates(self, s):
+        """
+        :type s: str
+        :rtype: str
+        """
+        stack = []
+        for i in s:
+            if stack and stack[-1] == i:
+                stack.pop()
+            else:
+                stack.append(i)
+        return "".join(stack)
